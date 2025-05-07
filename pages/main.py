b964fc86171e142ef7e4d248459bbd708e837585
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-05-07 13:49:21 +0700

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -37,6 +37,10 @@ st.markdown("""
     color:#2e5339;
 }
 
+[data-testid="stFileUploadDropzone"] {
+    width: 50%;
+}
+
 [data-testid="stFileDropzoneInstructions"] small:nth-of-type(1), [data-testid="stUploadedFile"] small:nth-of-type(1) {
     color:#2e5339;
 }
